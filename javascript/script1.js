e6c35e0772e6aa509e0b679883d2765a267a67e8
Luu Thang <savenationwin@gmail.com>
2023-06-09 22:15:54 +0700

    update

---
+++ javascript/script1.js
@@ -0,0 +1,43 @@
+// var a = 18;
+// console.log("b", b);
+// var b;
+// for (let index = 0; index < 10; index++) {
+//     console.log("index", index);
+// }
+//  {
+//     let c = "thang";
+//     var d = "thang"
+//  }
+// // console.log("index after for", index);
+// console.log("d", d);
+
+// const str = `
+//     luu quoc thang
+//     0364384662
+//     ${a}
+// `;
+
+// var persion = {
+//     "full Name": "thang",
+//     age: 18,
+//     show: function() {
+//         console.log(`${this.fullName} - ${this.age}`)
+//     }
+// }
+// const arr = [1,2,3,4,5]
+// console.log(persion["full Name"]);
+// for (const i of arr) {
+//     console.log(i)
+// }
+
+// for (const i in persion) {
+//     console.log(persion[i])
+// }
+
+// console.log("str", str)
+// // str = "luuquocthang";
+
+
+// a[0] = 10;
+// // console.log("c", c);
+// console.log("file 1 loaded")
---
+++ javascript/script2.js
@@ -0,0 +1,58 @@
+var tbStudent = document.getElementById("tbStudent");
+var addStudentForm = document.getElementById("addStudentForm");
+var fullName = document.getElementById("name");
+var date = document.getElementById("date");
+var des = document.getElementById("des");
+var id = 4;
+function onDelete1(event) {
+    // alert("deleted")
+    console.log(event);
+    var currentBtn = event.target;
+    var td = currentBtn.parentNode;
+    var tr = td.parentNode;
+    var table = tr.parentNode;
+    console.log(tr)
+    table.removeChild(tr);
+    // tbStudent.deleteRow(1);
+}
+
+function onDelete(event) {
+    // alert("deleted")
+    console.log(event);
+    var currentBtn = event.target;
+    var td = currentBtn.parentNode;
+    var tr = td.parentNode;
+    
+    console.log(tr.innerHTML);
+    tbStudent.innerHTML = tbStudent.innerHTML.replace(tr.innerHTML, "");
+    console.log(tbStudent.innerHTML);
+}
+
+addStudentForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("add");
+    id = id + 1;
+    var row = tbStudent.insertRow(id);
+    var cellId = row.insertCell(0);
+    cellId.innerHTML = id;
+    var cellId = row.insertCell(1);
+    cellId.innerHTML = fullName.value;
+    var cellId = row.insertCell(2);
+    cellId.innerHTML = date.value;
+    var cellId = row.insertCell(3);
+    cellId.innerHTML = des.value;
+    var cellId = row.insertCell(4);
+    cellId.innerHTML = `<button onclick="onDelete(event)">Del</button>`;
+
+
+    var row = `
+        <tr>
+            <td>${id}</td>
+            <td>${fullName.value}</td>
+            <td>${date.value}</td>
+            <td>${des.value}</td>
+            <td><button onclick="onDelete(event)">Del</button></td>
+        </tr>
+    `
+    
+})
